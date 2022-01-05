b9108f2349cb849663156bb56ca45e4b70206f8d
maverick-vrn <maverick-vrn@users.noreply.github.com>
2022-01-05 18:20:33 +0300

    refactor

--- script.js
+++ script.js
@@ -42,16 +42,24 @@ function update(cell, type)
 
 
  function check_win(field) {
-     if ((field[0] == field[1]) && (field[1] == field[2])) {alert('You Win');}
-     if ((field[3] == field[4]) && (field[4] == field[5])) {alert('You Win');}
-     if ((field[6] == field[7]) && (field[7] == field[8])) {alert('You Win');}
+    //  if ((field[0] == field[1]) && (field[1] == field[2])) {alert('You Win');}
+    //  if ((field[3] == field[4]) && (field[4] == field[5])) {alert('You Win');}
+    //  if ((field[6] == field[7]) && (field[7] == field[8])) {alert('You Win');}
      
-     if ((field[0] == field[3]) && (field[3] == field[6])) {alert('You Win');}
-     if ((field[1] == field[4]) && (field[4] == field[7])) {alert('You Win');}
-     if ((field[2] == field[5]) && (field[5] == field[8])) {alert('You Win');}
+    //  if ((field[0] == field[3]) && (field[3] == field[6])) {alert('You Win');}
+    //  if ((field[1] == field[4]) && (field[4] == field[7])) {alert('You Win');}
+    //  if ((field[2] == field[5]) && (field[5] == field[8])) {alert('You Win');}
 
-     if ((field[0] == field[4]) && (field[4] == field[8])) {alert('You Win');}
-     if ((field[2] == field[4]) && (field[4] == field[6])) {alert('You Win');}
+    //  if ((field[0] == field[4]) && (field[4] == field[8])) {alert('You Win');}
+    //  if ((field[2] == field[4]) && (field[4] == field[6])) {alert('You Win');}
+
+     var win = [[0,1,2],[3,4,5],[6,7,8],
+                [0,3,6],[1,4,7],[2,5,8],
+                [0,4,8],[2,4,6]];
+     win.forEach(element => {
+         if ( (field[element[0]] == field[element[1]]) && (field[element[1]] == field[element[2]]) )
+         {alert('You Win ! ! !');}
+     });
  }
 
 function fun()
@@ -105,6 +113,6 @@ setInterval(()=>{
         /*  Прячем предыдущую
             If нужен для первого прохода - работает и без него  */
      }
-},3000)
+},2000)
         
     
